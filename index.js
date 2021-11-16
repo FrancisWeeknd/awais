e7a6fb85de785df6cdd2d6245466d887a80b43bb
Francis <vishnujack4@gmail.com>
2021-11-16 21:08:19 +0530

    Added Chalk Colors

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const pkgJson = require("./package.json");
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const log = console.log;
+const twitterClr = chalk.hex( "#1da1f2" ).bold.inverse;
+const githubClr = chalk.hex( "#6cc644" ).bold.inverse;
+const dim = chalk.dim;
 
 welcome({
   title: `${pkgJson.name}`,
@@ -14,4 +19,28 @@ welcome({
   clear: true,
 });
 
-console.log(`Francis - Who am I?`);
+log(`
+${chalk.bgBlue.bold(` Francis - Ordinary guy `)}
+
+${chalk.italic(`I have no clue where my life is heading!`)}
+
+${githubClr(` GitHub `)} ${dim(`https://github.com/Francis`)}
+${twitterClr(` Twitter `)} ${dim(`https://twitter.com`)}
+`);
+
+log( chalk.green.underline( `This is green` ) )
+
+log()
+
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+const error = chalk.bold.red;
+const warning = chalk.keyword("orange");
+
+console.log(error("Error!"));
+console.log(warning("Warning!"));
