8b841f541d9cf9ff1b85cd73026963ff03f0d5f1
Francis <vishnujack4@gmail.com>
2021-11-16 10:36:56 +0530

    Added Header CLI intro

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
-const clearConsole = require("clear-any-console");
+const pkgJson = require("./package.json");
+const welcome = require("cli-welcome");
 
-// Clears the console.
-clearConsole();
+welcome({
+  title: `${pkgJson.name}`,
+  tagLine: `Howdy, nice to meet you`,
+  description: `${pkgJson.description}`,
+  version: `${pkgJson.version}`,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
 
-console.clear()
-console.log( `Francis - Who am I?` )
+console.log(`Francis - Who am I?`);
